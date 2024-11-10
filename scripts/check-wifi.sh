9b10ff6aa15c7d4c10f21e9f0c944c10e5b24d7f
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-11 00:35:33 +0100

    configs: adjust cron every 5 minutes

--- scripts/check-wifi.sh
+++ scripts/check-wifi.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+# Original idea: https://www.weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
+
 LOG_DIR="/var/log/check-wifi"
 ROUTER_IP="192.168.1.1"
 
