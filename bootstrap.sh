bdf9fe67e44da0168e11f9f93e65afa77e44571f
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-10 23:00:45 +0100

    bootstrap: rename create_symbolic_links -> configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$BASEDIR"/bootstrap/file_system.sh
-"$BASEDIR"/bootstrap/create_symbolic_links.sh "$BASEDIR"
+"$BASEDIR"/bootstrap/configs.sh "$BASEDIR"
 "$BASEDIR"/bootstrap/dependencies.sh
