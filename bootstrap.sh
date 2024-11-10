50be2151a597cd892ee8541f4f93680a7920a5f2
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-11 00:13:22 +0100

    bootstrap: run configs.sh as root

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$BASEDIR"/bootstrap/file_system.sh
-"$BASEDIR"/bootstrap/configs.sh "$BASEDIR"
+sudo "$BASEDIR"/bootstrap/configs.sh "$BASEDIR"
 "$BASEDIR"/bootstrap/dependencies.sh
