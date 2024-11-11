e87d3bb4572ecb253c4e83e1f52478242267eddf
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-11 22:40:22 +0100

    scripts: fix typo

--- scripts/check-wifi.sh
+++ scripts/check-wifi.sh
@@ -10,6 +10,6 @@ ROUTER_IP="192.168.1.1"
 if ! ping -c4 "$ROUTER_IP" > /dev/null; then
     now=$(date '+%Y-%m-%d %H:%M:%S %Z')
     sudo mkdir -p "$LOG_DIR"
-    echo "[$now] wifi disconneted" | sudo tee "$LOG_DIR/history.log" > /dev/null
+    echo "[$now] wifi disconnected -> reboot" | sudo tee "$LOG_DIR/history.log" > /dev/null
     sudo shutdown -r now
 fi
