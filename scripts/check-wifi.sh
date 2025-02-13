329e01cfd197487636a16c6a403cf29aac2c6e95
Cosimo Matteini <dev.matteini@gmail.com>
2025-02-13 22:29:55 +0100

    scripts: add temperature to check-wifi log

--- scripts/check-wifi.sh
+++ scripts/check-wifi.sh
@@ -7,9 +7,26 @@ set -euo pipefail
 LOG_DIR="/var/log/check-wifi"
 ROUTER_IP="192.168.1.1"
 
+now(){
+    date '+%Y-%m-%d %H:%M:%S %Z'
+}
+
+restart_system(){
+    if [[ ${DEBUG-} != true  ]]; then
+        sudo shutdown -r now
+    else
+        echo "Simulating reboot"
+    fi
+}
+
+temperature(){
+    vcgencmd measure_temp | tr -d "temp="
+}
+
+echo "[$(now)] $(temperature)" | sudo tee -a "$LOG_DIR/execution.log"
+
 if ! ping -c3 "$ROUTER_IP" > /dev/null; then
-    now=$(date '+%Y-%m-%d %H:%M:%S %Z')
     sudo mkdir -p "$LOG_DIR"
-    echo "[$now] wifi disconnected -> reboot" | sudo tee -a "$LOG_DIR/history.log" > /dev/null
-    sudo shutdown -r now
+    echo "[$(now)] [$(temperature)] wifi disconnected -> reboot" | sudo tee -a "$LOG_DIR/history.log" > /dev/null
+    restart_system
 fi
